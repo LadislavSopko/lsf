5376714837bdeb0aa8ed1ee7a7b4b55bbe24a192
Ladislav Sopko <ladislav.sopko@gmail.com>
2025-05-05 20:09:45 +0200

    Added csharp

---
+++ implementations/csharp/Zerox.LSF/Zerox.LSF.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Zerox.LSF.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ implementations/csharp/Zerox.LSF/Zerox.LSF/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Zerox.LSF
+{
+    public class Class1
+    {
+
+    }
+}
